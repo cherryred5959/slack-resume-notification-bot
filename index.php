dce2c35017a3e0e436ed5fd28d7e00cd929a3212
cherry <cherryred5959@gmail.com>
2019-02-21 01:20:03 +0900

    타이머에서 동작하게 변경

--- index.php
+++ index.php
@@ -4,58 +4,40 @@ declare(strict_types = 1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use BotMan\BotMan\BotMan;
-use BotMan\BotMan\BotManFactory;
-use BotMan\Drivers\Slack\SlackDriver;
-use Workerman\Connection\TcpConnection;
+use Slack\Channel;
+use Slack\Message\Attachment;
+use Slack\Message\AttachmentField;
 use Workerman\Worker;
 use Workerman\Lib\Timer;
 use Dotenv\Dotenv;
-use BotMan\BotMan\Drivers\DriverManager;
 
 Dotenv::create(__DIR__)->load();
 
 $task = new Worker();
 $task->onWorkerStart = function () {
+    $loop = Worker::getEventLoop();
+
+    $client = new \Slack\ApiClient($loop);
+    $client->setToken(getenv('SLACK_TOKEN'));
     Timer::add(
         getenv('WORKERMAN_TIME_INTERVAL') ?? 300,
-        function () {
-            echo getenv('TIME_INTERVAL');
+        function () use ($client) {
+            $client->getChannelByName(getenv('SLACK_CHANNEL'))->then(function (Channel $channel) use ($client) {
+                $message = $client->getMessageBuilder()
+                    ->setChannel($channel)
+                    ->setText('Hello, all!')
+                    ->addAttachment(new Attachment('My Attachment', 'attachment text'))
+                    ->addAttachment(new Attachment('Build Status', 'Build failed! :/', 'build failed', 'danger'))
+                    ->addAttachment(new Attachment('Some Fields', 'fields', null, '#BADA55', [
+                            new AttachmentField('Title1', 'Text', false),
+                            new AttachmentField('Title2', 'Some other text', true)]
+                    ))
+                    ->create();
+
+                $client->postMessage($message);
+            });
         }
     );
 };
-
-DriverManager::loadDriver(SlackDriver::class);
-
-$port = getenv('WORKERMAN_PORT') ?? 8000;
-$http = new Worker("http://0.0.0.0:{$port}");
-$http->count = 1;
-$http->onMessage = function (TcpConnection $connection, array $data) {
-    $botman = BotManFactory::create([
-        'slack' => [
-            'token' => getenv('SLACK_TOKEN')
-        ],
-        null,
-        new \Symfony\Component\HttpFoundation\Request(
-            $data['get'],
-            $data['post'],
-            [],
-            $data['cookie'],
-            $data['files'],
-            $data['server'],
-            []
-        )
-    ]);
-
-   /* $botman->fallback(function (BotMan $bot) {
-        $bot->reply('I heard you! :)');
-    });*/
-
-    $botman->listen();
-    $botman->reply('test');
-
-    $connection->send("hello world \n");
-};
-
 
 Worker::runAll();
